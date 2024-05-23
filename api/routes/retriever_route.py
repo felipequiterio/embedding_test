bd4c7bfa5df5cf6bdcb43ff38a5d8601dd71f672
Felipe Quiterio <felipe.quiterio@ip.tv>
2024-05-23 15:30:55 -0300

    Commenting large embeddings routes

--- api/routes/retriever_route.py
+++ api/routes/retriever_route.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from services.embeddings import get_embeddings, get_embeddings_large
-from services.retrievers import retrieve_passages_cosine, retrieve_passages_ip, retrieve_passages_l2, \
-    retrieve_passages_cosine_large, retrieve_passages_ip_large, retrieve_passages_l2_large
+from services.embeddings import get_embeddings
+from services.retrievers import retrieve_passages_cosine, retrieve_passages_ip, retrieve_passages_l2
+
 
 route = APIRouter(
     tags=["Passage Retrieval"]
@@ -16,13 +16,13 @@ async def retrieve_cosine(query, k):
     passages = response.passages
     return {'message': 'Similar passages retrieved!', 'Passages': passages}
 
-
-@route.post("/retrieve_cosine_large/")
-async def retrieve_cosine(query, k):
-    query_vector = get_embeddings_large(query)
-    response = retrieve_passages_cosine_large(query_vector, k)
-    passages = response.passages
-    return {'message': 'Similar passages retrieved!', 'Passages': passages}
+#
+# @route.post("/retrieve_cosine_large/")
+# async def retrieve_cosine(query, k):
+#     query_vector = get_embeddings_large(query)
+#     response = retrieve_passages_cosine_large(query_vector, k)
+#     passages = response.passages
+#     return {'message': 'Similar passages retrieved!', 'Passages': passages}
 
 
 @route.post("/retrieve_ip/")
@@ -32,13 +32,13 @@ async def retrieve_inner_product(query, k):
     passages = response.passages
     return {'message': 'Similar passages retrieved!', 'Passages': passages}
 
-
-@route.post("/retrieve_ip_large/")
-async def retrieve_inner_product(query, k):
-    query_vector = get_embeddings_large(query)
-    response = retrieve_passages_ip_large(query_vector, k)
-    passages = response.passages
-    return {'message': 'Similar passages retrieved!', 'Passages': passages}
+#
+# @route.post("/retrieve_ip_large/")
+# async def retrieve_inner_product(query, k):
+#     query_vector = get_embeddings_large(query)
+#     response = retrieve_passages_ip_large(query_vector, k)
+#     passages = response.passages
+#     return {'message': 'Similar passages retrieved!', 'Passages': passages}
 
 
 @route.post("/retrieve_l2/")
@@ -48,11 +48,11 @@ async def retrieve_l2(query, k):
     passages = response.passages
     return {'message': 'Similar passages retrieved!', 'Passages': passages}
 
-
-
-@route.post("/retrieve_l2_large/")
-async def retrieve_l2(query, k):
-    query_vector = get_embeddings_large(query)
-    response = retrieve_passages_l2_large(query_vector, k)
-    passages = response.passages
-    return {'message': 'Similar passages retrieved!', 'Passages': passages}
+#
+#
+# @route.post("/retrieve_l2_large/")
+# async def retrieve_l2(query, k):
+#     query_vector = get_embeddings_large(query)
+#     response = retrieve_passages_l2_large(query_vector, k)
+#     passages = response.passages
+#     return {'message': 'Similar passages retrieved!', 'Passages': passages}
