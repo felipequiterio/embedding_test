979c52ae55b94232445973af2653a86fa828ffac
Felipe Quiterio <felipe.quiterio@ip.tv>
2024-05-23 14:58:13 -0300

    Implementing function to create large embedding database

--- database/create.py
+++ database/create.py
@@ -31,6 +31,31 @@ def create_text_vector_table():
         disconnect_database(conn)
 
 
+def create_text_vector_table_large():
+    try:
+        conn = connect_database()
+        cur = conn.cursor()
+        cur.execute("""
+                CREATE EXTENSION IF NOT EXISTS vector;
+                """)
+        logger.info('Extension created.')
+        conn.commit()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS text_vector_embeddings_large (
+                id SERIAL PRIMARY KEY,
+                question TEXT,
+                embedding vector(1024)
+            );
+        """)
+        conn.commit()
+        logger.info("Table 'text_vector_embeddings_large' created successfully.")
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error("Error creating table: ", error)
+    finally:
+        disconnect_database(conn)
+
+
 def create_extension():
     try:
         conn = connect_database()
