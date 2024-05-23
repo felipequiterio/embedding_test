5decbf778798a3d7d5e55c88f55fe1034a6cf526
Felipe Quiterio <felipe.quiterio@ip.tv>
2024-05-23 15:32:10 -0300

    Commenting retrieevers for large model

--- services/retrievers.py
+++ services/retrievers.py
@@ -41,34 +41,34 @@ def retrieve_passages_cosine(query_or_queries: Union[str, List[str]], k: Optiona
         if conn:
             conn.close()
 
-
-def retrieve_passages_cosine_large(query_or_queries: Union[str, List[str]], k: Optional[int] = None):
-    conn = connect_database()
-    if not conn:
-        return dspy.Prediction(passages=[])
-
-    try:
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        logger.info('Retrieving passages with cosine distance')
-        sql_query = """
-            SELECT question
-            FROM text_vector_embeddings_large
-            ORDER BY embedding <=> %s::vector
-            LIMIT %s;
-        """
-        cur.execute(sql_query, (query_or_queries, k))
-        results = cur.fetchall()
-        passages = [{'question': result['question']} for result in
-                    results]
-        cur.close()
-        logger.info('Passages retrieved.')
-        return dspy.Prediction(passages=passages)
-    except psycopg2.Error as e:
-        logger.error(f"Failed to retrieve data: {e}")
-        return dspy.Prediction(passages=[])
-    finally:
-        if conn:
-            conn.close()
+#
+# def retrieve_passages_cosine_large(query_or_queries: Union[str, List[str]], k: Optional[int] = None):
+#     conn = connect_database()
+#     if not conn:
+#         return dspy.Prediction(passages=[])
+#
+#     try:
+#         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#         logger.info('Retrieving passages with cosine distance')
+#         sql_query = """
+#             SELECT question
+#             FROM text_vector_embeddings_large
+#             ORDER BY embedding <=> %s::vector
+#             LIMIT %s;
+#         """
+#         cur.execute(sql_query, (query_or_queries, k))
+#         results = cur.fetchall()
+#         passages = [{'question': result['question']} for result in
+#                     results]
+#         cur.close()
+#         logger.info('Passages retrieved.')
+#         return dspy.Prediction(passages=passages)
+#     except psycopg2.Error as e:
+#         logger.error(f"Failed to retrieve data: {e}")
+#         return dspy.Prediction(passages=[])
+#     finally:
+#         if conn:
+#             conn.close()
 
 
 def retrieve_passages_l2(query_or_queries: Union[str, List[str]], k: Optional[int] = None):
@@ -102,37 +102,37 @@ def retrieve_passages_l2(query_or_queries: Union[str, List[str]], k: Optional[in
         if conn:
             conn.close()
 
-
-def retrieve_passages_l2_large(query_or_queries: Union[str, List[str]], k: Optional[int] = None):
-    conn = connect_database()
-    if not conn:
-        return dspy.Prediction(passages=[])
-
-    try:
-        logger.info('Retrieving passages with L2 distance')
-
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-        sql_query = """
-            SELECT question
-            FROM text_vector_embeddings_large
-            ORDER BY embedding <-> %s::vector
-            LIMIT %s;
-        """
-        cur.execute(sql_query, (query_or_queries, k))
-        results = cur.fetchall()
-        passages = [{'question': result['question']} for result in
-                    results]
-        cur.close()
-        logger.info('Passages retrieved.')
-
-        return dspy.Prediction(passages=passages)
-    except psycopg2.Error as e:
-        logger.error(f"Failed to retrieve data: {e}")
-        return dspy.Prediction(passages=[])
-    finally:
-        if conn:
-            conn.close()
+#
+# def retrieve_passages_l2_large(query_or_queries: Union[str, List[str]], k: Optional[int] = None):
+#     conn = connect_database()
+#     if not conn:
+#         return dspy.Prediction(passages=[])
+#
+#     try:
+#         logger.info('Retrieving passages with L2 distance')
+#
+#         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#
+#         sql_query = """
+#             SELECT question
+#             FROM text_vector_embeddings_large
+#             ORDER BY embedding <-> %s::vector
+#             LIMIT %s;
+#         """
+#         cur.execute(sql_query, (query_or_queries, k))
+#         results = cur.fetchall()
+#         passages = [{'question': result['question']} for result in
+#                     results]
+#         cur.close()
+#         logger.info('Passages retrieved.')
+#
+#         return dspy.Prediction(passages=passages)
+#     except psycopg2.Error as e:
+#         logger.error(f"Failed to retrieve data: {e}")
+#         return dspy.Prediction(passages=[])
+#     finally:
+#         if conn:
+#             conn.close()
 
 
 def retrieve_passages_ip(query_or_queries: Union[str, List[str]], k: Optional[int] = None):
@@ -165,33 +165,33 @@ def retrieve_passages_ip(query_or_queries: Union[str, List[str]], k: Optional[in
         if conn:
             conn.close()
 
-
-def retrieve_passages_ip_large(query_or_queries: Union[str, List[str]], k: Optional[int] = None):
-    conn = connect_database()
-    if not conn:
-        return dspy.Prediction(passages=[])
-
-    try:
-        logger.info('Retrieving passages with inner product distance')
-
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-        sql_query = """
-            SELECT question
-            FROM text_vector_embeddings_large
-            ORDER BY embedding <#> %s::vector
-            LIMIT %s;
-        """
-        cur.execute(sql_query, (query_or_queries, k))
-        results = cur.fetchall()
-        passages = [{'question': result['question']} for result in
-                    results]
-        cur.close()
-        logger.info('Passages retrieved.')
-        return dspy.Prediction(passages=passages)
-    except psycopg2.Error as e:
-        logger.error(f"Failed to retrieve data: {e}")
-        return dspy.Prediction(passages=[])
-    finally:
-        if conn:
-            conn.close()
+#
+# def retrieve_passages_ip_large(query_or_queries: Union[str, List[str]], k: Optional[int] = None):
+#     conn = connect_database()
+#     if not conn:
+#         return dspy.Prediction(passages=[])
+#
+#     try:
+#         logger.info('Retrieving passages with inner product distance')
+#
+#         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#
+#         sql_query = """
+#             SELECT question
+#             FROM text_vector_embeddings_large
+#             ORDER BY embedding <#> %s::vector
+#             LIMIT %s;
+#         """
+#         cur.execute(sql_query, (query_or_queries, k))
+#         results = cur.fetchall()
+#         passages = [{'question': result['question']} for result in
+#                     results]
+#         cur.close()
+#         logger.info('Passages retrieved.')
+#         return dspy.Prediction(passages=passages)
+#     except psycopg2.Error as e:
+#         logger.error(f"Failed to retrieve data: {e}")
+#         return dspy.Prediction(passages=[])
+#     finally:
+#         if conn:
+#             conn.close()
