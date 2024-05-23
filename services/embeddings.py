a9a3bc7763c8f96eb3f04733e9b5bffc667a41d2
Felipe Quiterio <felipe.quiterio@ip.tv>
2024-05-23 14:59:21 -0300

    Implementing embeddings large, using BERTimbau large model

--- services/embeddings.py
+++ services/embeddings.py
@@ -12,9 +12,19 @@ def load_tokenizer():
     return AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased')
 
 
+@log_time('Loading tokenizer large')
+def load_tokenizer_large():
+    return AutoTokenizer.from_pretrained('neuralmind/bert-large-portuguese-cased')
+
+
 @log_time('Loading model')
 def load_model():
     return AutoModel.from_pretrained('neuralmind/bert-base-portuguese-cased')
+
+
+@log_time('Loading model')
+def load_model_large():
+    return AutoModel.from_pretrained('neuralmind/bert-large-portuguese-cased')
 
 
 @log_time('Tokenizing text')
@@ -33,7 +43,10 @@ def generate_embeddings(model, input_ids):
 
 
 tokenizer = load_tokenizer()
+tokenizer_large = load_tokenizer_large()
+
 model = load_model()
+model_large = load_model_large()
 
 
 def get_embeddings(text):
@@ -44,3 +57,11 @@ def get_embeddings(text):
     embedding = cls_embedding.tolist()
     return embedding
 
+
+def get_embeddings_large(text):
+    logger.info(f'Starting to process text: "{text}"')
+    input_ids = tokenize_text(tokenizer_large, text)
+    cls_embedding = generate_embeddings(model_large, input_ids)
+    logger.info(f'Embeddings dimension: {cls_embedding.size()}')
+    embedding = cls_embedding.tolist()
+    return embedding
