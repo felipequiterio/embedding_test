9949b500d0201a04577bc2c277690f5fcf11e82b
Felipe Quiterio <felipe.quiterio@ip.tv>
2024-05-24 16:38:25 -0300

    Adding data to new table

--- add_data.py
+++ add_data.py
@@ -1,19 +1,19 @@
 from database.insert import insert_data
-from services.embeddings import get_embeddings, get_embeddings_large
+from services.embeddings import get_embeddings
 from utils.reader import read_csv
 
 
 def main():
-    questions = read_csv('data/sample_docs/question_dataset.csv')
+    questions = read_csv('data/sample_docs/teste.csv')
 
     data_to_insert = []
     for i, question in enumerate(questions):
         print(f'Adding data {i} of {len(questions)}')
-        embedding = get_embeddings_large(question)
+        embedding = get_embeddings(question)
         data_to_insert.append((question, embedding))
 
-    table_name = "text_vector_embeddings_large"
+    table_name = "text_vector_embeddings_testecsv"
     columns = ["question", "embedding"]
     insert_data(table_name, columns, data_to_insert)
 
-#main()
+main()
