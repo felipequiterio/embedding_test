7b0976102c1aac4acab9fa5fc2f1b672e51e6900
Felipe Quiterio <felipe.quiterio@ip.tv>
2024-05-24 16:37:59 -0300

    IMplementing new database for ambiguos questions

--- database/create.py
+++ database/create.py
@@ -56,6 +56,30 @@ def create_text_vector_table_large():
         disconnect_database(conn)
 
 
+def create_text_vector_table_testecsv():
+    try:
+        conn = connect_database()
+        cur = conn.cursor()
+        cur.execute("""
+                CREATE EXTENSION IF NOT EXISTS vector;
+                """)
+        logger.info('Extension created.')
+        conn.commit()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS text_vector_embeddings_testecsv (
+                id SERIAL PRIMARY KEY,
+                question TEXT,
+                embedding vector(768)
+            );
+        """)
+        conn.commit()
+        logger.info("Table 'text_vector_embeddings' created successfully.")
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error("Error creating table: ", error)
+    finally:
+        disconnect_database(conn)
+
 def create_extension():
     try:
         conn = connect_database()
--- database/setup.py
+++ database/setup.py
@@ -1,12 +1,13 @@
-from database.create import create_text_vector_table, create_text_vector_table_large
+from database.create import create_text_vector_table, create_text_vector_table_large, create_text_vector_table_testecsv
 from database.table import list_tables, describe_table, fetch_and_print_data
 
 create_text_vector_table()
 create_text_vector_table_large()
+create_text_vector_table_testecsv()
 
 list_tables()
 
-describe_table('text_vector_embeddings')
+#describe_table('text_vector_embeddings')
 
 # truncate_table('text_vector_embeddings')
 
